787869d37e2715d7fe9752a30b5f7601624c6808
Sergey Lebedev <sergey.a.lebedev@gmail.com>
2019-06-25 08:01:09 +0300

    detect HCA distance and choose net device

--- ext-net/ucx/nccl_ucx.c
+++ ext-net/ucx/nccl_ucx.c
@@ -1,4 +1,6 @@
+
 /*************************************************************************
+ * Copyright (c) 2016-2018, NVIDIA CORPORATION. All rights reserved.
  * Copyright (C) Mellanox Technologies Ltd. 2019.  ALL RIGHTS RESERVED.
  *
  * See LICENSE.txt for license information
@@ -10,6 +12,7 @@
 #include "nccl_net.h"
 #include "core.h"
 #include "socket.h"
+#include "ibvwrap.h"
 
 #include <assert.h>
 #include <stdio.h>
@@ -36,6 +39,31 @@
 ncclDebugLogger_t ucx_log_function = NULL;
 static const ucp_tag_t tag  = 0xABADBABE;
 static const ucp_tag_t tag_mask = 0;//0xFFFFFFFFFFFFFFFF; //recieve any message
+
+#define MAXNAMESIZE 64
+struct ncclIbDev {
+  int device;
+  uint8_t port;
+  uint8_t link;
+  struct ibv_context* context;
+  char devName[MAXNAMESIZE];
+};
+
+#define MAX_IB_PORT 15
+struct userIbDev {
+  char devName[MAXNAMESIZE];
+  uint16_t port_en;
+};
+
+
+#define MAX_IB_DEVS 16
+
+struct ncclIbDev ncclIbDevs[MAX_IB_DEVS];
+struct userIbDev userIbDevs[MAX_IB_DEVS];
+
+static int ncclNIbDevs = -1;
+pthread_mutex_t ncclIbLock = PTHREAD_MUTEX_INITIALIZER;
+
 
 ucp_context_h ucp_context;
 ucp_worker_h ucp_worker;
@@ -113,40 +141,99 @@ static ncclResult_t get_socket_addr(union socketAddress* addr) {
 static pthread_mutex_t worker_mutex;
 
 __hidden ncclResult_t ucx_init(ncclDebugLogger_t logFunction) {
-  int status;
-  ucp_params_t ucp_params;
-  ucp_config_t *config;
 
   ucx_log_function = logFunction;
-  UCXCHECK(ucp_config_read(NULL, NULL, &config));
-  memset(&ucp_params, 0, sizeof(ucp_params));
-  ucp_params.field_mask = UCP_PARAM_FIELD_FEATURES |
-                          UCP_PARAM_FIELD_REQUEST_SIZE |
-                          UCP_PARAM_FIELD_REQUEST_INIT;
-  ucp_params.features = UCP_FEATURE_TAG;
-  ucp_params.request_size = sizeof(struct ucx_request);
-  ucp_params.request_init = request_init;
-  UCXCHECK(ucp_init(&ucp_params, config, &ucp_context));
-  ucp_worker = (ucp_worker_h)(uintptr_t)UCP_WORKER_NONE;
-  ucp_config_release(config);
-
-  if (findInterfaces(if_name, &nccl_ucx_if_addr, MAX_IF_NAME_SIZE, 1) != 1) {
+  if (ncclNIbDevs == -1) {
+    pthread_mutex_lock(&ncclIbLock);
+    wrap_ibv_fork_init();
+    if (ncclNIbDevs == -1) {
+      ncclNIbDevs = 0;
+      if (findInterfaces(if_name, &nccl_ucx_if_addr, MAX_IF_NAME_SIZE, 1) != 1) {
         NCCL_UCX_WARN("NET/UCX : No IP interface found.");
         return ncclInternalError;
-   }
-  NCCL_UCX_INFO(NCCL_NET, "Using OOB %s", if_name);
+      }
+
+      // Detect IB cards
+      int nIbDevs;
+      struct ibv_device** devices;
+
+      // Check if user defined which IB device:port to use
+      char* userIbEnv = getenv("NCCL_IB_HCA");
+      struct netIf userIfs[MAX_IB_DEVS];
+      int searchNot = userIbEnv && userIbEnv[0] == '^';
+      int nUserIfs = parseStringList(userIbEnv, userIfs, MAX_IB_DEVS);
+
+      if (ncclSuccess != wrap_ibv_get_device_list(&devices, &nIbDevs)) return ncclInternalError;
+
+      for (int d=0; d<nIbDevs; d++) {
+        struct ibv_context * context;
+        if (ncclSuccess != wrap_ibv_open_device(&context, devices[d]) || context == NULL) {
+          NCCL_UCX_WARN("NET/UCX : Unable to open device %s", devices[d]->name);
+          continue;
+        }
+        int found = 0;
+        struct ibv_device_attr devAttr;
+        if (ncclSuccess != wrap_ibv_query_device(context, &devAttr)) {
+          NCCL_UCX_WARN("NET/UCX : Unable to query device %s", devices[d]->name);
+          if (ncclSuccess != wrap_ibv_close_device(context)) { return ncclInternalError; }
+          continue;
+        }
+        for (int port = 1; port <= devAttr.phys_port_cnt; port++) {
+          struct ibv_port_attr portAttr;
+          if (ncclSuccess != wrap_ibv_query_port(context, port, &portAttr)) {
+            WARN("NET/UCX : Unable to query port %d", port);
+            continue;
+          }
+          if (portAttr.state != IBV_PORT_ACTIVE) continue;
+          if (portAttr.link_layer != IBV_LINK_LAYER_INFINIBAND
+              && portAttr.link_layer != IBV_LINK_LAYER_ETHERNET) continue;
+
+          // check against user specified HCAs/ports
+          if (! (matchIfList(devices[d]->name, port, userIfs, nUserIfs) ^ searchNot)) {
+            continue;
+          }
+          ncclIbDevs[ncclNIbDevs].device = d;
+          ncclIbDevs[ncclNIbDevs].port = port;
+          ncclIbDevs[ncclNIbDevs].link = portAttr.link_layer;
+          ncclIbDevs[ncclNIbDevs].context = context;
+          strncpy(ncclIbDevs[ncclNIbDevs].devName, devices[d]->name, MAXNAMESIZE);
+          ncclNIbDevs++;
+          found++;
+          //pthread_create(&ncclIbAsyncThread, NULL, ncclIbAsyncThreadMain, context);
+        }
+        if (found == 0 && ncclSuccess != wrap_ibv_close_device(context)) { return ncclInternalError; }
+      }
+      if (nIbDevs && (ncclSuccess != wrap_ibv_free_device_list(devices))) { return ncclInternalError; };
+    }
+    if (ncclNIbDevs == 0) {
+      INFO(NCCL_INIT|NCCL_NET, "NET/UCX : No device found.");
+    } else {
+      char line[1024];
+      line[0] = '\0';
+      for (int d=0; d<ncclNIbDevs; d++) {
+        snprintf(line+strlen(line), 1023-strlen(line), " [%d]%s:%d/%s", d, ncclIbDevs[d].devName,
+                 ncclIbDevs[d].port, ncclIbDevs[d].link == IBV_LINK_LAYER_INFINIBAND ? "IB" : "RoCE");
+      }
+      line[1023] = '\0';
+      char addrline[1024];
+      INFO(NCCL_INIT|NCCL_NET, "NET/UCX : Using%s ; OOB %s", line, if_name);
+    }
+    pthread_mutex_unlock(&ncclIbLock);
+  }
+  ucp_worker = (ucp_worker_h)(uintptr_t)UCP_WORKER_NONE;
 
   //pthread_mutex_init(&worker_mutex, NULL);
   return ncclSuccess;
 }
 
 __hidden ncclResult_t ucx_devices(int* ndev) {
-  *ndev = 1; return ncclSuccess;
+  *ndev = ncclNIbDevs;
+  return ncclSuccess;
 }
 
 __hidden ncclResult_t ucx_pci_path(int dev, char** path) {
   char devicepath[PATH_MAX];
-  snprintf(devicepath, PATH_MAX, "/sys/class/infiniband/%s/device", "mlx5_0");
+  snprintf(devicepath, PATH_MAX, "/sys/class/infiniband/%s/device", ncclIbDevs[dev].devName);
   *path = realpath(devicepath, NULL);
   if (*path == NULL) {
     NCCL_UCX_WARN("Could not find real path of %s", devicepath);
@@ -161,10 +248,39 @@ __hidden ncclResult_t ucx_ptr_support(int dev, int* supported_types) {
   return ncclSuccess;
 }
 
+static ncclResult_t ucx_init_context(int dev){
+  ucp_params_t ucp_params;
+  ucp_config_t *config;
+  
+  if (ucp_context == NULL){
+    char ucx_prefix[PATH_MAX]; //DEV_####
+    char ucx_env_var[PATH_MAX]; //UCX_DEV_####_NET_DEVICES
+    char ucx_env_val[PATH_MAX]; //e.g. mlx5_0:1
+    
+    snprintf(ucx_prefix, PATH_MAX, "DEV_%d", dev);
+    snprintf(ucx_env_var, PATH_MAX, "UCX_%s_NET_DEVICES", ucx_prefix);
+    snprintf(ucx_env_val, PATH_MAX, "%s:%d" , ncclIbDevs[dev].devName, ncclIbDevs[dev].port);
+    setenv(ucx_env_var, ucx_env_val, 0);
+    UCXCHECK(ucp_config_read(ucx_prefix, NULL, &config));
+    
+    memset(&ucp_params, 0, sizeof(ucp_params));
+    ucp_params.field_mask = UCP_PARAM_FIELD_FEATURES |
+      UCP_PARAM_FIELD_REQUEST_SIZE |
+      UCP_PARAM_FIELD_REQUEST_INIT;
+    ucp_params.features = UCP_FEATURE_TAG;
+    ucp_params.request_size = sizeof(struct ucx_request);
+    ucp_params.request_init = request_init;
+    UCXCHECK(ucp_init(&ucp_params, config, &ucp_context));
+    ucp_config_release(config);
+  }
+}
+
 __hidden ncclResult_t ucx_listen(int dev, void* handle, void** listen_comm) {
   ucp_worker_params_t worker_params;
   ucx_listen_handle *my_handle;
   ucx_listen_comm *comm;
+
+  ucx_init_context(dev);
   
   //  pthread_mutex_lock(&worker_mutex);
   //  NCCL_UCX_INFO(NCCL_NET, "ucx_listen");
@@ -189,11 +305,10 @@ __hidden ncclResult_t ucx_listen(int dev, void* handle, void** listen_comm) {
   comm->worker = ucp_worker;
   *listen_comm = comm;
 
-  
   return ncclSuccess;
 }
 
-static void wait(ucp_worker_h ucp_worker, struct ucx_request *request){
+static void worker_wait(ucp_worker_h ucp_worker, struct ucx_request *request){
     while (request->completed == 0) {
         ucp_worker_progress(ucp_worker);
     }
@@ -229,6 +344,7 @@ __hidden ncclResult_t ucx_connect(int dev, void* handle, void** send_comm) {
     memset(&worker_params, 0, sizeof(worker_params));
     worker_params.field_mask  = UCP_WORKER_PARAM_FIELD_THREAD_MODE;
     worker_params.thread_mode = UCS_THREAD_MODE_SINGLE;
+    ucx_init_context(dev);
     UCXCHECK(ucp_worker_create(ucp_context, &worker_params, &ucp_worker));   
   }
   UCXCHECK(ucp_ep_create(ucp_worker, &ep_params, &ep));
@@ -262,7 +378,6 @@ __hidden ncclResult_t ucx_accept(void* listen_comm, void** recv_comm) {
   socketSend(r_comm->fd, my_addr, local_addr_len);
   ucp_worker_release_address(ucp_worker, my_addr);
   *recv_comm = r_comm;
-
 
   return ncclSuccess;
 }
@@ -348,7 +463,7 @@ __hidden ncclResult_t ucx_flush(void* recv_comm, void* data, int size, void* mha
   if (UCS_PTR_IS_ERR(req)) {
     fprintf(stderr, "Error executing ucp_worker_flush operation\n");
   } else if (UCS_PTR_STATUS(req) != UCS_OK) {
-    wait(ucp_worker, req);
+    worker_wait(ucp_worker, req);
     req->completed = 0; req->release = 0;
     ucp_request_release(req);
   }
